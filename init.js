2b9c9190635b205fdbc12b9c186c242106c44ce7
Divya <divvyatripaathi@gmail.com>
2020-11-29 09:13:21 +0100

    to-do

---
+++ init.js
@@ -0,0 +1,17 @@
+function AddBtn() {
+    let el = document.createElement('li')
+    el.innerHTML = document.getElementById("addText").value
+    document.getElementById("list").appendChild(el)
+}
+
+function myBtn() {
+    var ele = document.getElementById("demo");
+    ele.remove();
+
+}
+
+
+function myFun() {
+    var elmnt = document.getElementById("dem");
+    elmnt.remove();
+}
